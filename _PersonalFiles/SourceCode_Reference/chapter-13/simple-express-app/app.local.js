6340dd09b138c784754b938a5c85df260dd83f85
palash <palashkantisen@gmail.com>
2020-02-02 09:00:11 +1300

    removed some files

--- _PersonalFiles/SourceCode_Reference/chapter-13/simple-express-app/app.local.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-require("dotenv").config();
-const app = require('./app')
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`App listening on port ${port}`))
